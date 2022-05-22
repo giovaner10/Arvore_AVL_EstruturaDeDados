24092f4704e57279c5c4ae712aaa5cd7d3494205
giovaner10 <giovanerr10@gmail.com>
2022-05-22 11:21:05 -0300

    Arvor AVL

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+
+    public static void main (String args[]){
+        AvlTree t = new AvlTree();
+        t.insert(1);
+        t.insert(2);
+        t.insert(3);
+        t.insert(4);
+        t.insert(5);
+        t.insert(6);
+        t.insert(7);
+        t.insert(8);
+        t.insert(9);
+        t.displayTree();
+    }
+}
